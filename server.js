8fdd038d8a9f8f3b2d8f65f1ca63c99e44e9bdd0
Zach White <zechnwhite@gmail.com>
2019-11-03 15:39:02 -0800

    gets further

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
+const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
 
 
 const app = express();
+dotenv.config();
 const uri = process.env.MONGODB_URI;
 
 // Bodyparser Middleware
 app.use(bodyParser.json());
+
+//Cors Middleare
+app.use(cors());
 
 // Connect to MongoDB
 mongoose
@@ -18,3 +25,13 @@ mongoose
   })
   .then(() => console.log('MongoDB Connected...'))
   .catch(() => console.log(err));
+
+// Use Routes
+app.use('/api/items', require('./routes/api/items'));
+
+
+
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server listening on port ${port}`));
