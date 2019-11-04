8a99ec51c4b3ac2a3f9b8ee32e19d2be8b4bcd07
Zach White <zechnwhite@gmail.com>
2019-11-03 19:28:58 -0800

    all CRUD operations working with db

--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -7,7 +7,7 @@ import PropTypes from 'prop-types';
 import {
     getItems,
     deleteItem
-} from '../actions/ItemActions';
+} from '../actions/itemActions';
 
 class ShoppingList extends Component {
 
@@ -25,14 +25,14 @@ class ShoppingList extends Component {
             <Container>
                 <ListGroup>
                     <TransitionGroup className='shopping-list'>
-                        {items.map(({ id, name }) => (
-                            <CSSTransition key={id} timeout={500} classNames='fade'>
+                        {items.map(({ _id, name }) => (
+                            <CSSTransition key={_id} timeout={500} classNames='fade'>
                                 <ListGroupItem>
                                 <Button 
                                     className='remove-btn'
                                     color='danger'
                                     size='sm'
-                                    onClick={this.onDeleteClick.bind(this, id)}
+                                    onClick={this.onDeleteClick.bind(this, _id)}
                                     >&times;
                                 </Button>
                                     {name}
