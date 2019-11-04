224ffcf3b2a86df8a01a6a00dcab782b0772837c
Zach White <zechnwhite@gmail.com>
2019-11-03 19:37:02 -0800

    prepares for heroku deploy

--- server.js
+++ server.js
@@ -30,7 +30,14 @@ mongoose
 app.use('/api/items', require('./routes/api/items'));
 
 
-
+// Serve static assests if in production
+if(process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
 
 const port = process.env.PORT || 5000;
 
