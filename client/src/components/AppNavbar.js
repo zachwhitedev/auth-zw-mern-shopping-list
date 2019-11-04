11f60eba56954ac069f86080a483a7537342e3ba
Zach White <zechnwhite@gmail.com>
2019-11-03 20:40:18 -0800

    test-change

--- client/src/components/AppNavbar.js
+++ client/src/components/AppNavbar.js
@@ -31,7 +31,7 @@ class AppNavbar extends Component {
                         <Collapse isOpen={this.state.isOpen} navbar>
                             <Nav className='ml-auto' navbar>
                                 <NavItem>
-                                    <NavLink href='https://zachwhite.dev'>Portfolio</NavLink>
+                                    <NavLink href='https://www.zachwhite.dev'>Portfolio</NavLink>
                                 </NavItem>
                             </Nav>
                         </Collapse>
