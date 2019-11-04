fed6945dc31994a05e7b9f7807d519a01b3e9b41
Zach White <zechnwhite@gmail.com>
2019-11-03 21:33:09 -0800

    changes name

--- client/src/components/AppNavbar.js
+++ client/src/components/AppNavbar.js
@@ -26,7 +26,7 @@ class AppNavbar extends Component {
             <div>
                 <Navbar color="dark" dark expand="sm" className="mb-5">
                     <Container>
-                        <NavbarBrand href='/'>Shopping List</NavbarBrand>
+                        <NavbarBrand href='/'>Zach's List</NavbarBrand>
                         <NavbarToggler onClick={this.toggle}/>
                         <Collapse isOpen={this.state.isOpen} navbar>
                             <Nav className='ml-auto' navbar>
